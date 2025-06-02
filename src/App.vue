2f7e38ae9f9009c4899be213e95b9df35975365e
zhaoguangxu <zhaoguangxu801@gmail.com>
2025-06-03 01:30:28 +0800

    chore: 在 App.vue 中添加了调试信息 "1234567890"

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ onMounted(() => {
   <header>
     <p>Count: {{ count }}</p>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    1234567890
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 

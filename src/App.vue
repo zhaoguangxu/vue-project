b6328c21fe9efe4c4fef055010f3dccbdbf81b72
zhaoguangxu <zhaoguangxu801@gmail.com>
2025-06-02 16:52:17 +0800

    feat: 在 App.vue 中添加了计数器显示

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ onMounted(() => {
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    {{ count }}
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
